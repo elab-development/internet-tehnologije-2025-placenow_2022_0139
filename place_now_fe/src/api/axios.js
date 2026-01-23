878e51a8fd775db250a4d2a0dddfa0eddcfa8ca3
Una Cmiljanovic <unacmiljanovicc@gmail.com>
2026-01-23 22:37:39 +0100

    Definisan most izmedju klijentske i serverske strane preko Axios biblioteke

---
+++ place_now_fe/src/api/axios.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
